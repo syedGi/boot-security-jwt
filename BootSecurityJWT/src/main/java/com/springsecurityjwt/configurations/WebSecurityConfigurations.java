48c4af96f310ead35feb39b61caee5d497fcfbf4
syedGi <117427048+syedGi@users.noreply.github.com>
2023-12-23 22:42:38 +0530

    Update securityfilterchain method and commented

--- BootSecurityJWT/src/main/java/com/springsecurityjwt/configurations/WebSecurityConfigurations.java
+++ BootSecurityJWT/src/main/java/com/springsecurityjwt/configurations/WebSecurityConfigurations.java
@@ -2,13 +2,17 @@ package com.springsecurityjwt.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfigurations {
 
 	@Bean
@@ -31,4 +35,17 @@ public class WebSecurityConfigurations {
 				.and()
 				.build();
 	}
+	
+//	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//	    return httpSecurity
+//	        .csrf(csrf -> csrf.disable())
+//	        .authorizeHttpRequests(auth -> auth
+//	            .requestMatchers("/token/**").permitAll()
+//	            .anyRequest().authenticated()
+//	        )
+//	        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//	        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+//	        .httpBasic(Customizer.withDefaults())
+//	        .build();
+//	}
 }
